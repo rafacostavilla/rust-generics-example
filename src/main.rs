937923ed5e276acc25f36c4cd0625c258a9486e3
rafacostavilla <rafaelcostavilla@gmail.com>
2024-09-11 16:30:29 +1200

    Combine the two largest functions in only one using generics

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-fn largest_i32(list: &[i32]) -> &i32{
+fn _largest_i32(list: &[i32]) -> &i32{
     let mut largest = &list[0];
 
     for item in list{
@@ -7,7 +7,20 @@ fn largest_i32(list: &[i32]) -> &i32{
 
     &largest
 }
-fn largest_char(list: &[char]) -> &char{
+fn _largest_char(list: &[char]) -> &char{
+    let mut largest = &list[0];
+
+    for item in list{
+        if item > largest {largest = item;}
+    }
+
+    &largest
+}
+
+fn largest<T>(list: &[T]) -> &T
+where
+    T: PartialOrd
+{
     let mut largest = &list[0];
 
     for item in list{
@@ -20,12 +33,12 @@ fn largest_char(list: &[char]) -> &char{
 fn main() {
     let number_list = vec![1, 2, 3, 4, 5];
 
-    let result = largest_i32(&number_list);
+    let result = largest(&number_list);
     println!("The largest number is {result}");
     
     let char_list = vec!['a', 'b', 'c', 'd', 'e'];
     
-    let result = largest_char(&char_list);
+    let result = largest(&char_list);
     println!("The largest number is '{result}'");
     
 }
